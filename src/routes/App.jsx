ac2d356866b7ed1dd3fa7391dbf31a4df601f3c5
Josué Ramírez <josueramirez0110@gmail.com>
2021-12-27 18:53:16 -0600

    Added add to cart feature

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -11,26 +11,31 @@ import CreateAccount from '@pages/CreateAccount';
 import Order from '@pages/Order';
 import Checkout from '@pages/Checkout';
 import NotFound from "@pages/NotFound";
+import AppContext from '@context/AppContext';
+import useInitialState from '@hooks/useInitialState';
 import '@styles/global.css';
 
 const App = () => {
+  const initialState = useInitialState();
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route exact path="/" element={<Home />}/>
-          <Route exact path="/login" element={<Login />}/>
-          <Route exact path="/password-recovery" element={<PasswordRecovery />}/>
-          <Route exact path="/send-email" element={<SendEmail />}/>
-          <Route exact path="/new-password" element={<NewPassword />}/>
-          <Route exact path="/account" element={<Account />}/>
-          <Route exact path="/signup" element={<CreateAccount />}/>
-          <Route exact path="/order" element={<Order />}/>
-          <Route exact path="/checkout" element={<Checkout />}/>
-          <Route path="*" element={<NotFound />}/>
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <AppContext.Provider value={initialState}>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route exact path="/" element={<Home />}/>
+            <Route exact path="/login" element={<Login />}/>
+            <Route exact path="/password-recovery" element={<PasswordRecovery />}/>
+            <Route exact path="/send-email" element={<SendEmail />}/>
+            <Route exact path="/new-password" element={<NewPassword />}/>
+            <Route exact path="/account" element={<Account />}/>
+            <Route exact path="/signup" element={<CreateAccount />}/>
+            <Route exact path="/order" element={<Order />}/>
+            <Route exact path="/checkout" element={<Checkout />}/>
+            <Route path="*" element={<NotFound />}/>
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </AppContext.Provider>
   );
 }
 
